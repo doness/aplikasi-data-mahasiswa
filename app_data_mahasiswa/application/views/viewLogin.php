ec32221ad6f4ffee35a59482cb25afb46d7e8834
Irfan Tanjung <irfantanjung@hotmail.com>
2015-07-23 07:46:12 +0700

    Update viewLogin.php

--- app_data_mahasiswa/application/views/viewLogin.php
+++ app_data_mahasiswa/application/views/viewLogin.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Database Mahasiswa Unikom Ver 1.0</title>
+    <title>Aplikasi Database Mahasiswa Ver 1.0</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<base href="<?php echo base_url();?>"/>
